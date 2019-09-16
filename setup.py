d4ac5915b69b84e25dec8f9429c93a1ef4d8ca29
Sam Bradley <sam.bradley@csiro.au>
2019-09-16 15:45:21 +0800

    Added json file to the package

--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.0.1'
+version = "0.0.1"
 
 setup(
-    name='ckanext-repeating',
+    name="ckanext-repeating",
     version=version,
     description="Repeating fields for CKAN",
     long_description="""
     """,
-    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    keywords='',
-    author='Government of Canada',
-    author_email='Michel.Gendron@statcan.gc.ca',
-    url='',
-    license='MIT',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    namespace_packages=['ckanext'],
+    classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords="",
+    author="Government of Canada",
+    author_email="Michel.Gendron@statcan.gc.ca",
+    url="",
+    license="MIT",
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+    namespace_packages=["ckanext"],
     include_package_data=True,
+    package_data={},
     zip_safe=False,
     install_requires=[
         # -*- Extra requirements: -*-
     ],
-    entry_points=\
-    """
+    entry_points="""
     [ckan.plugins]
     repeating=ckanext.repeating.plugins:RepeatingPlugin
     """,
